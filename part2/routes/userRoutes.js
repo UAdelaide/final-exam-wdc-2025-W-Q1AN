36fee176c2ffea0630c9235a619259067e173dd2
W-Q1AN <a1916517@adelaide.edu.au>
2025-06-20 13:26:29 +0000

    [automatic commit] 2025-06-20 13:26:29 UTC 1.147.96.151

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -28,41 +28,6 @@ router.post('/register', async (req, res) => {
   }
 });
 
-// POST a new user (simple login)
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const [rows] = await db.query(`
-      SELECT user_id, username, role FROM Users
-      WHERE username = ? AND password_hash = ?
-    `, [username, password]);
-
-    if (rows.length === 0) {
-      return res.status(401).json({ error: 'Invalid credentials' });
-    }
-
-    // store session
-    req.session.user = {
-      id: rows[0].user_id,
-      username: rows[0].username,
-      role: rows[0].role
-    };
-
-    // return role
-    res.json({
-      message: 'Login successful',
-      user: {
-        username: rows[0].username,
-        role: rows[0].role
-      }
-    });
-
-  } catch (error) {
-    res.status(500).json({ error: 'Login failed' });
-  }
-});
-
 router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
