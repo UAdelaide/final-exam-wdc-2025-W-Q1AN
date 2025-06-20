454f6231a421f15a8cc54810ecad6333472a7735
W-Q1AN <a1916517@adelaide.edu.au>
2025-06-20 13:43:51 +0000

    [automatic commit] 2025-06-20 13:43:51 UTC 1.147.96.151

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,7 +37,7 @@ router.get('/me', (req, res) => {
 
 // POST login
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body; // use username password
+  const { username, password } = req.body;
 
   try {
     const [rows] = await db.query(`
